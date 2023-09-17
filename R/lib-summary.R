03fbec664c4fada4905b2bb261fee1c97977e0de
ritika-giri <ritika.giri3@gmail.com>
2023-09-17 13:41:32 -0500

    Added documentation

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,7 +1,16 @@
+#' R Library Summary
+#'
+#' Provide a brief summary of the package libraries on your machine
+#'
+#' @return A `data.frame` containing the count of packages in each of the user's libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
-  pkg_df # return
-}
+  return(pkg_df)
+  }
